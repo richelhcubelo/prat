4e884a525b31eaaf0bb2c23371d1cc2db2230d0d
Richel <cubelorichel@gmail.com>
2024-09-18 15:38:54 +0800

    shh

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "BISU",
+        short_name: "App",
+        description: "Your app description",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/icons/bisu.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/bisu.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
